0359b889ffb78a040ca9d8beee036208d9abafa9
mdbandi123 <mdsumang.091@gmail.com>
2021-05-18 23:23:04 +0800

    First commit

---
+++ mevnproject/src/components/EditComponent.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class = "row justify-content-center">
+     <div class = "col-md-8"> 
+         <div class = "card card-default">
+             <div class="card-header">Edit Component</div>
+             <div class="card-body">I'm the Edit Component compontent</div>
+         </div>
+     </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'EditComponent'
+}
+</script>
---
+++ mevnproject/src/components/IndexComponent.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class = "row justify-content-center">
+     <div class = "col-md-8"> 
+         <div class = "card card-default">
+             <div class="card-header">Index Component</div>
+             <div class="card-body">I'm the Index Component compontent</div>
+         </div>
+     </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'IndexComponent'
+}
+</script>
